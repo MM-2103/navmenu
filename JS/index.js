5ed0d2e37dbbb0eda07f9dedeb0338fa8649fe96
MM-2103 <mohsen.menem1@gmail.com>
2023-09-15 09:36:02 +0000

    decided to just store the json data in a JS variable

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,11 @@
-fetch('../data.json')
-    .then((response) => response.json())
-    .then((data) => {
-        const home = data.title[0];
-        console.log("home");
-    }); 
+var jsonData = [
+    {"title": "home", "url": "/"},
+    {"title": "producten", "url": "/producten/"},
+    {"title": "Overons", "url": "/overons/"},
+    {"title": "contact", "url": "/contact/"}
+  ];
+
+var firstTitle = jsonData[0].title;
+console.log(firstTitle); // This will log "home"
+
+  
